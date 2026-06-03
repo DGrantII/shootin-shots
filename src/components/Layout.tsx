373dc11151045d2c6e3aca0b768e83bd8bf209c6
Devin Grant II <dj4096@icloud.com>
2026-06-03 18:40:07 -0500

    Add ScrollToTop component to Layout for improved navigation experience

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { ScrollToTop } from './ScrollToTop';
 import { SiteNav } from './SiteNav';
 import { SiteFooter } from './SiteFooter';
 
 export function Layout() {
   return (
     <>
+      <ScrollToTop />
       <SiteNav />
       <div className="content-card">
         <Outlet />
